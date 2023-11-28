db3a228893bc9541984c6af6157a5fb2a8589a48
simoIdbrahim <mohamedidbrahim16@gmail.com>
2023-11-28 06:14:54 -0800

    add

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -10,14 +10,10 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 {
 	size_t nodes = 0;
 
-	if (tree && (tree->left || tree->right))
+	if (tree)
 	{
-		nodes = 1;
-	}
-	else
-	{
-		nodes = 0;
-	}
+		if ((tree->left != NULL || tree->right != NULL))
+			nodes = 1;
 
 	nodes += binary_tree_nodes(tree->left);
 	nodes += binary_tree_nodes(tree->right);
