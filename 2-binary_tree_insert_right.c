2b79435acd4b6f3fd4d1f05a03c795957df60a27
simoIdbrahim <mohamedidbrahim16@gmail.com>
2023-11-28 05:17:34 -0800

    add

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,33 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_right - main function.
+ * @parent: arg one.
+ * @value: arg two.
+ * Return: NULL or pnt to the new node
+ */
+
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *newNode;
+
+	if (parent == NULL)
+	{
+		return (NULL);
+	}
+
+	newNode = binary_tree_node(parent, value);
+	if (newNode == NULL)
+	{
+		return (NULL);
+	}
+
+	if (parent->right != NULL)
+	{
+		newNode->right = parent->right;
+		parent->right->parent = newNode;
+	}
+	parent->right = newNode;
+
+	return (newNode);
+}
