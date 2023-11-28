08a505ec206a86341dca1eb8c61fc7fde564d31d
simoIdbrahim <mohamedidbrahim16@gmail.com>
2023-11-28 06:32:29 -0800

    add

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,36 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_balance - function one.
+ * @tree: arg one.
+ * Return: NULL or 0 else balance factor.
+ */
+
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	if (tree)
+
+		treePos = binary_tree_height(tree->left) - binary_tree_height(tree->right)
+		return (treePos);
+
+	return (0);
+}
+
+/**
+ * binary_tree_height - function two
+ * @tree: arg one.
+ * Return: NULL or 0 else height.
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	if (tree)
+	{
+		size_t x = 0, xx = 0;
+
+		x = tree->left ? 1 + binary_tree_height(tree->left) : 1;
+		xx = tree->right ? 1 + binary_tree_height(tree->right) : 1;
+		return ((x > xx) ? x : xx);
+	}
+	return (0);
+}
