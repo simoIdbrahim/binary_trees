93e09a11d31b03de54149eb67b09d6cf9fb7b650
simoIdbrahim <mohamedidbrahim16@gmail.com>
2023-11-28 06:37:30 -0800

    add

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -9,9 +9,9 @@
 int binary_tree_balance(const binary_tree_t *tree)
 {
 	if (tree)
-
-		treePos = binary_tree_height(tree->left) - binary_tree_height(tree->right)
-		return (treePos);
+	{
+		return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
+	}
 
 	return (0);
 }
