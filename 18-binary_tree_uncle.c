66f2160429955d674345a300614f33f0a8fddedf
simoIdbrahim <mohamedidbrahim16@gmail.com>
2023-11-30 12:20:54 -0800

    add

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_uncle - function one.
+ * @node: arg one.
+ * Return: node or null.
+ */
+
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	if (node == NULL ||
+	    node->parent == NULL ||
+	    node->parent->parent == NULL)
+	{
+		return (NULL);
+	}
+	if (node->parent->parent->left == node->parent)
+	{
+		return (node->parent->parent->right);
+	}
+	return (node->parent->parent->left);
+}
